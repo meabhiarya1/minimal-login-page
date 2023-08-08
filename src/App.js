cea4cfcfc0ee32362123660cb58f5b92b79bc79d
Abhishek kumar <abhi.arya97161@gmail.com>
2023-08-08 11:43:27 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 function App() {
+
+  const handleEmail = (e) =>{
+    console.log(e.target.value)
+  }
+
+  const handlePass = (e) =>{
+    console.log(e.target.value)
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    alert("Logged In")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box component="form" noValidate autoComplete="off">
+      <div className="container_white">
+        <div className='container_padding'>
+          <h3 className='container_title'>Back to your digital life</h3>
+          <p className='container_para'>Choose one of the option to go</p>
+          <div className='input_card'>
+            <TextField onChange={handleEmail} className='input'
+              id="email"
+              label="Email"
+              type='email'
+              variant="outlined" />
+
+            <TextField onChange={handlePass} className='input'
+              id="pass"
+              label="Password"
+              type='password'
+              variant="outlined" />
+
+          </div>
+
+          <div className='line'>
+            <p className='line_para'>Or continue with</p>
+          </div>  
+
+
+          <div className='social_card'>
+
+          <div className='apple'>
+              <img className='social_img' alt='' src='/1.png'/>
+            </div>
+
+            <div className='google'>
+              <img className='social_img' alt='' src='/2.png'/>
+            </div>
+
+            <div className='meta'>
+              <img className='social_img' alt='' src='/3.png'/>
+            </div>
+
+          </div>
+
+          <div className='card_button'>
+            <button className='button-28' type='submit' onClick={handleSubmit}>Login</button>
+          </div>
+        </div>
+      </div>
+    </Box>
+
   );
 }
 
